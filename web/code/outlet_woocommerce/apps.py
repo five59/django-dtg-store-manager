650451bb714f5db48ed58e8903258c4343d2aad7
Andrew Marconi <andrew@559labs.com>
2017-03-04 11:13:55 -0500

    Removed old files
    Removed stale files in preparation for migration to virtualenv.

--- web/code/outlet_woocommerce/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OutletWoocommerceConfig(AppConfig):
-    name = 'outlet_woocommerce'
-    verbose_name = "Outlet: WooCommerce"
--- web/code/vendor_printful/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'vendor_printful.apps.VendorPrintfulConfig'
